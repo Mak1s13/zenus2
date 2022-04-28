d7ed08718356ee69a7ab0cbabf4e0d99aeaad0f1
Mak1s13 <78659553+Mak1s13@users.noreply.github.com>
2022-04-28 15:03:40 +0300

    Add files via upload

---
+++ js/scr-reveal.js
@@ -0,0 +1,4 @@
+sr.reveal(`.logo`)
+sr.reveal(`.menu-list`)
+sr.reveal(`.titleMessage`, { delay: 400 })
+sr.reveal(`.aboutus`, { delay: 800 })
---
+++ js/smooth.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $("a[href*=#]").click(function(event){     
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+   
+   
+// document ready  
+});
+
